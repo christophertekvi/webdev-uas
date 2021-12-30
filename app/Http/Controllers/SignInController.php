5a3d84c39961e0256d32c1833ac390bae2bdbd56
AnandaSuryaWedhana <75228944+AnandaSuryaWedhana@users.noreply.github.com>
2021-12-30 14:41:29 +0800

    Create SignInController.php

---
+++ app/Http/Controllers/SignInController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class SignInController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+    public function authenticate(Request $request){
+        $request->validate([
+           'email' => ['required', 'email:dns'],
+           'password' => ['required'],
+       ]);
+       $email = $request->email;
+       $password = $request->password;
+       $userdata = DB::table('pembeli')->where('EMAIL', $email)->first();
+       $obj = get_object_vars($userdata);
+       if ($userdata) {
+                if ($password == $obj['PASSWORD']) {
+                   $request->session()->put('email',$request->email);
+                   return view('home-sign-in', ['title'=>'home']);
+                } else {
+                   return back()->with('LoginError', 'login Failed');
+                }
+       } else {
+            return back()->with('LoginError', 'login Failed');
+       }
+   }
+}
