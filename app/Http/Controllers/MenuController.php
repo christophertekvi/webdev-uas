0d2a2d17e8917cc781ea0706947f66a1a87675da
shalomeiravw <shalomeiravalencia@gmail.com>
2022-01-08 23:26:15 +0700

    Revert "Merge branch 'main' of https://github.com/christophertekvi/webdev-uas"
    This reverts commit 84697c4b8d2f4297dfd0c1f61484cb62b0237e20, reversing
    changes made to bf796ac270f6a21800c745ab344acbf89a3dcbff.

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -34,6 +34,7 @@ class MenuController extends Controller
         $menu = Menu::query()->findOrFail($id);
 
         return view("detailmenuu", [
+            "nama" => $menu -> NAMA_ITEM,
             "m" => $menu
         ]);
     }
--- resources/views/partials/cssdetailmenu.blade.php
+++ resources/views/partials/cssdetailmenu.blade.php
@@ -1,6 +1,6 @@
 <head>
-    <link rel="stylesheet" href="css/detailmenu.css" />
-    <link rel="stylesheet" href="css/template.css" />
-    <script type="text/javascript" src="js/detail.js"></script>
-    <script type="text/javascript" src="js/template.js"></script>
+    <link rel="stylesheet" href="css\detailmenu.css" />
+    <link rel="stylesheet" href="css\template.css" />
+    <script type="text/javascript" src="js\detail.js"></script>
+    <script type="text/javascript" src="js\template.js"></script>
 </head>
