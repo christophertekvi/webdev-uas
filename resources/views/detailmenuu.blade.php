fe31bbeb8e02846a2517193fd7f5cf39f1eb8988
felicia graciella <fgraciella@student.ciputra.ac.id>
2022-01-08 23:13:28 +0700

    det. menu

--- resources/views/detailmenuu.blade.php
+++ resources/views/detailmenuu.blade.php
@@ -17,20 +17,15 @@
     <script type="text/javascript" src="js/detail.js"></script>
     <script type="text/javascript" src="js/template.js"></script>
 
+
+
+
     <link rel="stylesheet" href="css/bootstrap.min.css" />
     <link rel="stylesheet" href="css/bootstrap.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.1/mdb.min.css" rel="stylesheet"/>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet" />
 
-    {{-- <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <script>
-        $( function() {
-          $( "#datepicker" ).datepicker({ minDate: -20, maxDate: "+1M +15D" });
-        } );
-    </script> --}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.1/mdb.min.js" ></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous" ></script>
@@ -59,7 +54,7 @@
         <div  class="navbar-collapse collapse w-100" id="collapsingNavbar3">
             <ul class="navbar-nav w-100 justify-content-center">
                 <li class="nav-item active">
-                    <a class="nav-link" aria-current="page" href="/">HOME</a>
+                    <a class="nav-link" aria-current="page" href="/home-sign-in">HOME</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="/menu" >MENU</a>
@@ -145,16 +140,9 @@
         </div>
     </div>
 
-
-
-
-
-
-        <button id="btnScrollTop" onclick="scrollToTop()">
-          <i class="material-icons">keyboard_double_arrow_up</i>
-        </button>
-      </div>
-    </div>
+    <button id="btnScrollTop" onclick="scrollToTop()">
+        <i class="material-icons">keyboard_double_arrow_up</i>
+    </button>
   </body>
 
   <!-- Footer -->
