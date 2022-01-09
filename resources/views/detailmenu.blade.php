83610d82699b306e952203c56f86526326431d43
Louver <cleonard@student.ciputra.ac.id>
2022-01-09 14:24:36 +0700

    detail menu

--- resources/views/detailmenu.blade.php
+++ resources/views/detailmenu.blade.php
@@ -1,7 +1,34 @@
 <!DOCTYPE html>
 
 <html lang="en">
-  @include('partials.head-home')
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>MakanYuk | {{ $title }}</title>
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Outfit&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Outfit&display=swap" rel="stylesheet"/>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Material+Icons"/>
+
+        <link rel="icon" href="image/icon-logo.png" type="image/png">
+
+        <link rel="stylesheet" href="css/detailmenu.css" />
+        <link rel="stylesheet" href="css/template.css" />
+        <script type="text/javascript" src="js/home.js"></script>
+        <script type="text/javascript" src="js/template.js"></script>
+
+        <link rel="stylesheet" href="css/bootstrap.min.css" />
+        <link rel="stylesheet" href="css/bootstrap.css" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.1/mdb.min.css" rel="stylesheet"/>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet" />
+
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.1/mdb.min.js" ></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous" ></script>
+      </head>
 
   <body style="background-color:floralwhite;">
     <!--topbar-->
@@ -20,60 +47,10 @@
       </div>
     </div>
 
-    <div class="topbar">
-      <nav class="navbar navbar-custom navbar-expand-md bg-transparent justify-content-center">
-        <a href="/" class="navbar-brand d-flex w-50 mr-auto"></a>
-        <div  class="navbar-collapse collapse w-100" id="collapsingNavbar3">
-            <ul class="navbar-nav w-100 justify-content-center">
-                <li class="nav-item active">
-                    <a class="nav-link" aria-current="page" href="/">HOME</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/menu" >MENU</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/about-sign-in">ABOUT US</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/faqs-sign-in">FAQS</a>
-                </li>
-            </ul>
-            <ul class="nav navbar-nav ml-auto w-100 justify-content-end">
-                <a class="text-reset me-3" href="/cart">
-                  <i style="color: #ffffff;" class="fas fa-shopping-cart"></i>
-                </a>
-                <a
-                  style="padding-right: 18%;"
-                  class="dropdown-toggle d-flex align-items-center hidden-arrow"
-                  href="#"
-                  id="navbarDropdownMenuLink"
-                  role="button"
-                  data-mdb-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                <i class="fas fa-user" style="color: #ffffff;"></i>
-                </a>
-                <ul
-                  class="dropdown-menu dropdown-menu-end"
-                  aria-labelledby="navbarDropdownMenuLink"
-                >
-                  <li>
-                    <a style="text-align: left; color: black;" class="dropdown-item" href="/profile-signin">My Profile</a>
-                  </li>
-                  <li>
-                    <a style="text-align: left; color: black;" class="dropdown-item" >Points : 1000</a>
-                  </li>
-                  <li>
-                    <a style="text-align: left; color: black;" class="dropdown-item" href="/home">Sign Out</a>
-                  </li>
-                </ul>
-            </ul>
-        </div>
-      </nav>
-    </div>
+    @include('partials.navbar')
 
     <h1 style="text-align: center; margin: 50px 100px 50px 100px; font-family: Outfit, sans-serif;">
-        DETAIL
+        DETAIL MENU
     </h1>
 
 
