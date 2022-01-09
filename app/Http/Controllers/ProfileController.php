939759d411c10ee7363bd96067d6311bcb2de495
Louver <cleonard@student.ciputra.ac.id>
2022-01-09 13:26:33 +0700

    coba coba

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use Symfony\Component\HttpKernel\Profiler\Profile;
 
 class HomeController extends Controller
