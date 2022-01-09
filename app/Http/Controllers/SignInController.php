8b1f9b715174890bb3946afe7fa1e48aba809f85
shalomeiravw <shalomeiravalencia@gmail.com>
2022-01-09 23:17:41 +0700

    Update SignInController.php

--- app/Http/Controllers/SignInController.php
+++ app/Http/Controllers/SignInController.php
@@ -107,16 +107,20 @@ class SignInController extends Controller
                     $request->session()->put('firstname', $obj['FIRST_NAME']);
                     $request->session()->put('lastname', $obj['LAST_NAME']);
                     $request->session()->put('foto', $obj['FOTO_PEMBELI']);
-
                     $idPembeli = $obj['ID_PEMBELI'];
+
                     $fav = DB::table('menu_favorit')->where('ID_PEMBELI', $idPembeli)->first();
-                    $obj = get_object_vars($fav);
-                    $request->session()->put('fav', $obj['ID_MENU']);
-
+                    if (!is_null($fav)) {
+                        $obj = get_object_vars($fav);
+                        $request->session()->put('fav', $obj['ID_MENU']);
+                    } else {
+                    }
                     $orders = DB::table('transaksi_beli')->where('ID_PEMBELI', $idPembeli)->first();
-                    $obj = get_object_vars($orders);
-                    $request->session()->put('orders', $obj['ID_TB']);
-
+                    if (!is_null($orders)) {
+                        $obj = get_object_vars($orders);
+                        $request->session()->put('orders', $obj['ID_TB']);
+                    } else {
+                    }
                     return view('home-sign-in', ['title' => 'home']);
                 } else {
                     return back()->with('LoginError', 'Sign In Failed');
