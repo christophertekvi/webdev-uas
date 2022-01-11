aabb15bd1a7d35b7a4c0dad3bfb9f2e081ba466d
Louver <cleonard@student.ciputra.ac.id>
2022-01-11 23:40:39 +0700

    malam yang pusing

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -16,10 +16,9 @@
     <link rel="stylesheet" href="css/checkout.css">
     <script type="text/javascript" src="js/template.js"></script>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link href="css/metro.css" rel="stylesheet">
+    <script src="js/jquery-2.1.3.min.js"></script>
+    <script src="js/metro.js"></script>
 
     <link rel="stylesheet" href="css/bootstrap.min.css" />
     <link rel="stylesheet" href="css/bootstrap.css" />
@@ -59,7 +58,10 @@
                     </div>
                 </div>
 
+                <div class="input-control text" data-role="datepicker">
+                    <input type="text" name="tanggalPesan" required/>
 
+                </div>
 
                 <div class="bagian2" style="margin-top: 30px;">
                     <h1 style="font-family: 'Outfit', sans-serif; font-size: 170%;">Alamat :</h1>
