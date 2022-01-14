ca64ba95956e3a3aafbb4e4a3d4111623cd0c5f2
AnandaSuryaWedhana <75228944+AnandaSuryaWedhana@users.noreply.github.com>
2022-01-14 12:44:23 +0800

    Update CheckoutController.php

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -36,6 +36,7 @@ class CheckoutController extends Controller
         $id = session('idPembeli');
 
         $genidtb = DB::select('select `GenIDTB`()');
+        $obj = get_object_vars($genidtb[0]);
         $type = ["COD", "Transfer Bank"];
         $tglKirim = $request->input("tanggalPesan");
         $alamat = $request->input("alamat");
@@ -48,18 +49,23 @@ class CheckoutController extends Controller
                     ->select('id_menu', 'nama_menu', 'foto_menu', 'harga_menu', 'qty')
                     ->where('id_pembeli' ,$id)
                     ->get();
-        $total = DB::table('keranjang')->select(DB::raw('sum(harga_menu*qty) as total'))->where('ID_PEMBELI', $id)->get();
+
+        $total = DB::table('keranjang')
+        ->select(DB::raw('sum(harga_menu*qty) as total'))
+        ->where('ID_PEMBELI', $id)
+        ->get();
+
 
         $poin = DB::table('pembeli')->select(DB::raw('poin'))->where('ID_PEMBELI', $id)->get();
         $totalbayar = ((int)$total[0]->total) - ((int)$poin[0]->poin) + $ongkir;
         $dapet = $totalbayar * 0.1;
         DB::table('transaksi_beli')->insert([
             'ID_PEMBELI' => $id,
-            'ID_TB' => $genidtb,
+            'ID_TB' => $obj['`GenIDTB`()'],
             'TANGGAL_TRANSAKSI' => now(),
-            'SUBTOTAL_TRANSAKSI' => $total,
-            'PAKE_POIN' => $poin,
-            'TANGGAL_KIRIM' => $tglKirim,
+            'SUBTOTAL_TRANSAKSI' => $total[0]->total,
+            'PAKE_POIN' => $poin[0]->poin,
+            'TANGGAL_KIRIM' => now(),
             'NOTES_PESAN' => $pesan,
             'ALAMAT' => $alamat,
             'ONGKIR' => $ongkir,
@@ -73,11 +79,11 @@ class CheckoutController extends Controller
         foreach($data as $d)
         {
             DB::table('detail_beli')->insert([
-                'ID_TB' => $genidtb,
+                'ID_TB' =>$obj['`GenIDTB`()'],
                 'ID_PEMBELI' => $id,
-                'ID_MENU' => $data -> id_menu,
-                'QUANTITY_BELI' => $data -> qty,
-                'SUBTOTAL' => ($data->harga_menu)*($data->qty),
+                'ID_MENU' => $d -> id_menu,
+                'QUANTITY_BELI' => $d -> qty,
+                'SUBTOTAL' => ($d->harga_menu)*($d->qty),
                 'RATING' => null,
                 'review' => null,
                 'DB_DELETE' => 0
